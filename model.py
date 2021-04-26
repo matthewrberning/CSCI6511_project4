910d5e2da40e4395a6b25df93c02241db6236c9d
grady mcpeak <gradymcpeak@gmail.com>
2021-04-25 21:23:36 -0400

    refined learn function

--- model.py
+++ model.py
@@ -13,6 +13,9 @@ def num_to_move(num):
         return 'W'
     return 'ERROR!'
 
+#TODO: actually update q table
+def update_q_table(table):
+    return table
 
 def learn(q_table, worldId=0, mode='train'):
 
@@ -23,47 +26,50 @@ def learn(q_table, worldId=0, mode='train'):
     #init terminal state
     terminal_state = False
 
+    #find out where we are
+    loc_response = a.locate_me()
+    #OK response looks like {"code":"OK","world":"0","state":"0:2"}
+    if loc_response["code"] != "OK":
+            print(f"something broke on locate_me call \nresponse lookes like: {loc_response}")
+            return -1
+    # convert JSON into a tuple
+    location = int(loc_response["state"].split(':')[0]), int(loc_response["state"].split(':')[1]) #location is a tuple (x, y)
 
     while True:
-        
-        loc_response = a.locate_me()
-        #OK response looks like {"code":"OK","world":"0","state":"0:2"}
+    
+        #in q-table, get index of best option for movement based on our current state in the world
+        move_num = np.argmax(q_table[location[0]][location[1]])
 
-        if loc_response["code"] != "OK":
-            print(f"something broke on locate_me call \nresponse lookes like: {loc_response}")
-            break
-
-        location = loc_response["state"]
-
-        x, y = int(location.split(':')[0]), int(location.split(':')[1])
-
-        #in q-table, get index of best option for movement
-        move_num = np.argmax(q_table[x][y])
-
-        #make the move
-        move_response = a.make_move(move=num_to_move(move_num), worldId='0') #TODO we might want to handle the worldId as a var inside the current api class instance i.e. api.world_ID
+        #make the move - transition into a new state
+        move_response = a.make_move(move=num_to_move(move_num), worldId='0') 
+        #OK response looks like {"code":"OK","worldId":0,"runId":"931","reward":-0.1000000000,"scoreIncrement":-0.0800000000,"newState":{"x":"0","y":3}}
         
         if move_response["code"] != "OK":
             print(f"something broke on make_move call \nresponse lookes like: {move_response}")
             break
+        # convert new location JSON into tuple
+        new_loc = int(move_response["newState"]["x"]), int(move_response["newState"]["y"]) #tuple (x,y) (PROBABLY DON'T NEED THIS!?)
+
+        reward = move_response["reward"]
+
+        #update the q-table for the state we were in before
+        update_q_table(location, q_table)
+        
+        #update our current location var
+        location = new_loc
+
+        #TODO: compute the temporal difference... somehow?
+
 
         if terminal_state:
             break
-    # while true (breaks when we hit a terminal state)
-    
-        # make a call to the api to find where we are 
-        x = 5
-        # chose an action based on where we are
 
-        # peform the action at the transition to the new state
-
-        # receive the reward and compute the Temporal Difference
-
-        # update the Q-Value for the previous state
 
 
     #close the api instance
     a.enter_world(worldId=-1)
+
+    return q_table
 
 
 def init_q_table():
