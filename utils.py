d4304ecd06739cf30be35f77a2443eb4345f36b6
mdt48 <mdt_@gwu.edu>
2021-04-28 17:41:52 -0400

    figure saving

--- utils.py
+++ utils.py
@@ -4,4 +4,4 @@ def plot_learning(worldId, epoch, cumulative_average, rn):
     plt.figure(2)
     plt.plot(cumulative_average)
     plt.xscale('log')
-    plt.savefig(f'runs/attempt-{rn}/world_{worldId}_epoch{epoch}learning.png')
+    plt.savefig(f'runs/world_{worldId}/attempt-{rn}/world_{worldId}_epoch{epoch}learning.png')
